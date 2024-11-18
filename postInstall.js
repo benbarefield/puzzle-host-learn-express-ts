3e3079955abf2636a163ff27f4fe5567504be310
Ben Barefield <ben.barefield@gmail.com>
2024-11-18 12:13:56 -0800

    fixing paths

--- postInstall.js
+++ postInstall.js
@@ -1,11 +1,11 @@
 const {cp, copyFile} = require("node:fs/promises");
+const path = require('node:path');
 
-// __dirname is relative to the file location, not where it is being run from
-console.log(__dirname)
-
-await cp("./src", "../../src");
-await cp("./test", "../../test");
-await cp("./types", "../../types");
-await copyFile("./jest.config.js", "../../jest.config.js");
-await copyFile("./README.md", "../../README.md");
-await copyFile("./tsconfig.json", "../../tsconfig.json");
+(async () => {
+  await cp(path.resolve(__dirname, "./src"), path.resolve(__dirname, "../../src"), { recursive: true });
+  await cp(path.resolve(__dirname, "./test"), path.resolve(__dirname, "../../test"), { recursive: true });
+  await cp(path.resolve(__dirname, "./types"), path.resolve(__dirname, "../../types"), { recursive: true });
+  await copyFile(path.resolve(__dirname, "./jest.config.js"), path.resolve(__dirname, "../../jest.config.js"));
+  await copyFile(path.resolve(__dirname, "./README.md"), path.resolve(__dirname, "../../README.md"));
+  await copyFile(path.resolve(__dirname, "./tsconfig.json"), path.resolve(__dirname, "../../tsconfig.json"));
+})();
