bb3e0b2de7b41b90c7aca2d0983d24e541534149
Ben Barefield <ben.barefield@gmail.com>
2024-11-18 11:37:36 -0800

    adding file copier

---
+++ postInstall.js
@@ -0,0 +1,11 @@
+import {cp, copyFile} from "node:fs/promises";
+
+// __dirname is relative to the file location, not where it is being run from
+console.log(__dirname)
+
+await cp("./src", "../../src");
+await cp("./test", "../../test");
+await cp("./types", "../../types");
+await copyFile("./jest.config.js", "../../jest.config.js");
+await copyFile("./README.md", "../../README.md");
+await copyFile("./tsconfig.json", "../../tsconfig.json");
